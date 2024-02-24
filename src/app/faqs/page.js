4fa79644309af9dfc555667035d0c3fc2725b7cb
Qboakye <qboakye@gmail.com>
2024-02-24 18:53:26 +0000

    updated FAQs page

--- src/app/faqs/page.js
+++ src/app/faqs/page.js
@@ -10,38 +10,38 @@ import { RiSearchLine, FiPhone, FiMail, FiArrowRight } from '../assets/icons/van
 
 import { faqAbout } from '../data/data';
 
-export default function Faqs(){
-    return(
+export default function Faqs() {
+    return (
         <>
-        <Navbar manuClass="navigation-menu nav-light nav-left" containerClass="container"/>
-        <section className="bg-half-170 d-table w-100" style={{backgroundImage:`url('/images/bg/department.jpg')`, backgroundPosition:'center'}}>
-            <div className="bg-overlay bg-overlay-dark"></div>
-            <div className="container">
-                <div className="row mt-5 justify-content-center">
-                    <div className="col-12">
-                        <div className="section-title text-center">
-                            <h2 className="sub-title text-white title-dark">How can we help you ?</h2>
-                        </div>
-                        <div className="subcribe-form text-center mt-4">
-                            <form>
-                                <input type="text" id="name" name="name" className="border bg-white-50 rounded-pill" required="" placeholder="Ask a questions..."/>
-                                <button type="submit" className="btn btn-pills btn-primary"><RiSearchLine className="mb-0 align-middle me-1"/> Search</button>
-                            </form>
-                            <p className="text-white-50 mb-0 para-desc mx-auto mt-3">*We are collect your searching keywords to improve our FAQ</p>
+            <Navbar manuClass="navigation-menu nav-light nav-left" containerClass="container" />
+            <section className="bg-half-170 d-table w-100" style={{ backgroundImage: `url('/images/bg/department.jpg')`, backgroundPosition: 'center' }}>
+                <div className="bg-overlay bg-overlay-dark"></div>
+                <div className="container">
+                    <div className="row mt-5 justify-content-center">
+                        <div className="col-12">
+                            <div className="section-title text-center">
+                                <h2 className="sub-title text-white title-dark">How can we help you ?</h2>
+                            </div>
+                            <div className="subcribe-form text-center mt-4">
+                                <form>
+                                    <input type="text" id="name" name="name" className="border bg-white-50 rounded-pill" required="" placeholder="Ask a questions..." />
+                                    <button type="submit" className="btn btn-pills btn-primary"><RiSearchLine className="mb-0 align-middle me-1" /> Search</button>
+                                </form>
+                                <p className="text-white-50 mb-0 para-desc mx-auto mt-3">*We collect your searching keywords to improve our FAQ</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </section>
+            <div className="position-relative">
+                <div className="shape overflow-hidden text-color-white">
+                    <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+                    </svg>
+                </div>
             </div>
-        </section>
-        <div className="position-relative">
-            <div className="shape overflow-hidden text-color-white">
-                <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
-                </svg>
-            </div>
-        </div>
-        <section className="section">
-            <div className="container">
+            <section className="section">
+                {/* <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12">
                         <div className="section-title text-center mb-4 pb-2">
@@ -70,72 +70,72 @@ export default function Faqs(){
                         )
                     })}
                 </div>
-            </div>
+            </div> */}
 
-            <div className="container mt-100 mt-60">
-                <div className="row justify-content-center">
-                    <div className="col-12">
-                        <div className="section-title text-center mb-4 pb-2">
-                            <h4 className="mb-4">General Questions</h4>
-                            <p className="para-desc mx-auto text-muted mb-0">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row justify-content-center">
-                    <div className="col-lg-9 mt-4 pt-2">
-                        <Accordion/>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container mt-100 mt-60">
-                <div className="row justify-content-center">
-                    <div className="col-12">
-                        <div className="section-title text-center mb-4 pb-2">
-                            <h4 className="mb-4">Still have a questions ?</h4>
-                            <p className="para-desc mx-auto text-muted mb-0">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row justify-content-center">
-                    <div className="col-lg-9">
-                        <div className="row">
-                            <div className="col-md-6 mt-4 pt-2">
-                                <div className="card border-0 text-center features feature-primary rounded">
-                                    <div className="icon text-center mx-auto rounded-md">
-                                        <FiPhone className="h3 mb-0"/>
-                                    </div>
-        
-                                    <div className="card-body p-0 mt-4">
-                                        <h5>Phone</h5>
-                                        <p className="text-muted">Great doctor if you need your family member to get effective assistance</p>
-                                        <Link href="tel:+152534-468-854" className="link">+152 534-468-854</Link>
-                                    </div>
-                                </div>
-                            </div>
-        
-                            <div className="col-md-6 mt-4 pt-2">
-                                <div className="card border-0 text-center features feature-primary rounded">
-                                    <div className="icon text-center mx-auto rounded-md">
-                                        <FiMail className="h3 mb-0"/>
-                                    </div>
-        
-                                    <div className="card-body p-0 mt-4">
-                                        <h5>Email</h5>
-                                        <p className="text-muted">Great doctor if you need your family member to get effective assistance</p>
-                                        <Link href="mailto:contact@example.com" className="link">contact@example.com</Link>
-                                    </div>
-                                </div>
+                <div className="container mt-50 mt-60"> {/* //changed margin top from 100 to 50 */}
+                    <div className="row justify-content-center">
+                        <div className="col-12">
+                            <div className="section-title text-center mb-4 pb-2">
+                                <h4 className="mb-4">General Questions</h4>
+                                <p className="para-desc mx-auto text-muted mb-0">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
                             </div>
                         </div>
                     </div>
+
+                    <div className="row justify-content-center">
+                        <div className="col-lg-9 mt-4 pt-2">
+                            <Accordion />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <Footer/>
-        <ScrollTop/>
+
+                <div className="container mt-100 mt-60">
+                    <div className="row justify-content-center">
+                        <div className="col-12">
+                            <div className="section-title text-center mb-4 pb-2">
+                                <h4 className="mb-4">Still have a questions ?</h4>
+                                <p className="para-desc mx-auto text-muted mb-0">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row justify-content-center">
+                        <div className="col-lg-9">
+                            <div className="row">
+                                <div className="col-md-6 mt-4 pt-2">
+                                    <div className="card border-0 text-center features feature-primary rounded">
+                                        <div className="icon text-center mx-auto rounded-md">
+                                            <FiPhone className="h3 mb-0" />
+                                        </div>
+
+                                        <div className="card-body p-0 mt-4">
+                                            <h5>Phone</h5>
+                                            <p className="text-muted">Great doctor if you need your family member to get effective assistance</p>
+                                            <Link href="tel:+152534-468-854" className="link">+152 534-468-854</Link>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6 mt-4 pt-2">
+                                    <div className="card border-0 text-center features feature-primary rounded">
+                                        <div className="icon text-center mx-auto rounded-md">
+                                            <FiMail className="h3 mb-0" />
+                                        </div>
+
+                                        <div className="card-body p-0 mt-4">
+                                            <h5>Email</h5>
+                                            <p className="text-muted">Great doctor if you need your family member to get effective assistance</p>
+                                            <Link href="mailto:contact@example.com" className="link">contact@example.com</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+            <ScrollTop />
         </>
     )
 }
